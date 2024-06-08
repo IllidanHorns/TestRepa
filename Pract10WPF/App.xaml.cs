5bd7646cc6e6aa7cfb9688e5fac22583972e1fea
IllidanHorns <illidanhorns13@gmail.com>
2024-06-08 12:56:09 +0300

    init

---
+++ Pract10WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Pract10WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
